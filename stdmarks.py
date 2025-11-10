d23bdc2dc8808eacfc19c544dd34624961eefc1f
SameerAhmad-07 <sameerhallikeri4@gmail.com>
2025-11-10 15:39:32 +0530

    Update stdmarks.py

--- stdmarks.py
+++ stdmarks.py
@@ -1,19 +1,24 @@
-marks = [85, 78, 92, 67, 74]  
+import sys
+if len(sys.argv) == 5:
+    marks = [float(m) for m in sys.argv[0:5]]
+    print("User provided marks values:")
+else:
+    marks = [80, 75, 90, 85, 70]
+    print("No input given - using default marks:")
+average = sum(marks) / 5
 
-average = sum(marks) / len(marks)
-
-if average >= 85:
+if average >= 90:
     grade = 'A'
-elif average >= 70:
+elif average >= 75:
     grade = 'B'
-elif average >= 55:
+elif average >= 60:
     grade = 'C'
 elif average >= 40:
     grade = 'D'
 else:
     grade = 'Fail'
 
-
-print(f"Marks: {marks}")
+print("\n--- Result ---")
+print("Marks:", marks)
 print(f"Average Marks: {average:.2f}")
 print(f"Grade: {grade}")
